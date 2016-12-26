4207e59493cf4e84d2573a532f0c8d26f7c57654
QHamzaE <es.hamza.sup@gmail.com>
2016-12-26 21:17:29 +0000

    init

---
+++ src/moodle/Cours.java
@@ -0,0 +1,26 @@
+package moodle;
+
+/**
+ * Created by hamza on 26/12/16.
+ */
+public class Cours {
+
+    protected int id;
+    protected String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/myagents/MoodleContainer.java
@@ -0,0 +1,29 @@
+package myagents;
+
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
+
+/**
+ * Created by hamza on 26/12/16.
+ */
+public class MoodleContainer {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        try {
+            Runtime runtime=Runtime.instance();
+            ProfileImpl profileImpl=new ProfileImpl(false);
+            profileImpl.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+            AgentContainer agentContainer=runtime.createAgentContainer(profileImpl);
+            AgentController agentController=agentContainer.createNewAgent
+                    ("MoodleAgent", MoodleAgent.class.getName(), new Object[]{});
+            agentController.start();
+        } catch (ControllerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
